d2abe43937b9cbcc289fb211caa2125b70f5dcb5
sdsddssdsd12321 <h098s02914@chunchon.gwe.hs.kr>
2025-11-17 12:26:55 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,7 +78,7 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
     labels[0]:{"texts":["김세정님은 존나 예쁩니다.진짜 말이 안나와요."],
               "videos":["https://www.youtube.com/watch?v=NkeExG9jEfM&list=RDNkeExG9jEfM&start_radio=1"]},
               "images":["https://talkimg.imbc.com/TVianUpload/tvian/TViews/image/2021/01/24/xn9O3dCeYZEI637470784911089604.JPG"]}
-    labels[1]:{"texts":["김지원님도 진짜 말이 안되게 예쁩니다. 개인적으로 약 9년정도 팬입니다."]},
+    labels[1]:{"texts":["김지원님도 진짜 말이 안되게 예쁩니다. 개인적으로 약 9년정도 팬입니다."],
               "videos":["https://www.youtube.com/watch?v=pEzCVjoV5BA"]},
     labels[2]:{"texts":["박은빈님도 굉장히 예쁘고 연기도 잘합니다."],
               "videos":["https://www.youtube.com/watch?v=YPfxjgJDKio"]},
